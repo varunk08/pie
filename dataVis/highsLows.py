97e88af645b35448094c00f3490e525952b8e2d8
varun <varunk08@users.noreply.github.com>
2019-03-18 22:45:27 -0400

    date line added

--- dataVis/highsLows.py
+++ dataVis/highsLows.py
@@ -1,21 +1,26 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     headerRow = next(reader)
 
-    highs=[]
+    dates, highs=[], []
     for row in reader:
-        highs.append(row[1])
+        currentDate = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(currentDate)
+        high = int(row[1])
+        highs.append(high)
 
     print(highs)
 
     fig = plt.figure(dpi=128, figsize=(10,6))
-    plt.plot(highs, c='red')
+    plt.plot(dates, highs, c='red')
     plt.title('Daily high  temperatures, July 2014', fontsize=24)
     plt.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
 
     plt.tick_params(axis='both', which='major', labelsize=16)
