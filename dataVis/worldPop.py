d6afa84be473a4df2d5f37b5a7f3f63f739ba33c
varun <varunk08@users.noreply.github.com>
2019-03-20 00:23:09 -0400

    data vis: fin world population data vis using pygal

--- dataVis/worldPop.py
+++ dataVis/worldPop.py
@@ -1,6 +1,7 @@
 import json
 from pygal.maps.world import COUNTRIES
 from pygal.maps.world import World
+from pygal.style import RotateStyle, LightColorizedStyle
 from countryCode import getCountryCode
 
 filename = 'population_data.json'
@@ -16,8 +17,21 @@ for popDict in popData:
         if code:
             ccPopulations[code] = population
 
+pops1, pops2, pops3 = {}, {}, {}
+for cc, pop in ccPopulations.items():
+	if pop < 10000000:
+		pops1[cc] = pop
+	elif pop < 1000000000:
+		pops2[cc] = pop
+	else:
+		pops3[cc] = pop
 
-wm = World()
+
+wm_lightStyle = LightColorizedStyle
+wm_style = RotateStyle('#336699', base_style=LightColorizedStyle)
+wm = World(style=wm_style)
 wm.title = "World Populations"
-wm.add('2010', ccPopulations)
+wm.add('0-10m', pops1)
+wm.add('10m-1bn', pops2)
+wm.add('>1bn', pops3)
 wm.render_to_file('worldPopulations.svg')
