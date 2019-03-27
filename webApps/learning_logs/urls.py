5028f30b184a005ec3aedd0550b582ce36088955
varun <varunk08@users.noreply.github.com>
2019-03-27 08:39:43 -0400

    webapps: add a new topic

--- webApps/learning_logs/urls.py
+++ webApps/learning_logs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('topics/', views.topics, name='topics'),
 	path('topics/<int:topic_id>/', views.topic, name='topic'),
+	path('new_topic/', views.new_topic, name='new_topic'),
 ]
