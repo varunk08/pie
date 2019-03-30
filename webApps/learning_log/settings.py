76c79a8b98f3a9e56f447d8c28beb17ebcd74327
varun <varunk08@users.noreply.github.com>
2019-03-30 14:05:20 -0400

    web app: ready for deployment on heroku

--- webApps/learning_log/settings.py
+++ webApps/learning_log/settings.py
@@ -130,3 +130,21 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
     'include_jquery' : 'True',
 }
+
+
+# Heroku settings
+cwd = os.getcwd() #gets current working directory
+if cwd =='/app' or cwd[:4] == '/tmp': # in heroku cwd is during deployment /app and during dev /tmp
+    import dj_database_url
+    DATABASES = {
+    'default' : dj_database_url.config(default='postgres://localhost')
+    }
+
+    SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO', 'https')
+
+    ALLOWED_HOSTS = ['*']
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static')),
