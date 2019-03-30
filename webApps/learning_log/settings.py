2ede11da1225a72dc1a5c0ceef412b02f05a9cce
varun <varunk08@users.noreply.github.com>
2019-03-30 12:31:25 -0400

    web apps: styling the learning log using bootstrap

--- webApps/learning_log/settings.py
+++ webApps/learning_log/settings.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-LOGIN_URL = '/users/login/'
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -38,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'bootstrap3',
 
     # My apps
     'learning_logs',
@@ -123,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/users/login/'
+
+BOOTSTRAP3 = {
+    'include_jquery' : 'True',
+}
